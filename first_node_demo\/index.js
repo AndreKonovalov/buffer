09d8986d60b02fc6d85f982e58f23d4934f4ddb4
AndreKonovalov <47122488+AndreKonovalov@users.noreply.github.com>
2020-11-13 13:26:05 +0300

    Update index.js

--- first_node_demo\/index.js
+++ first_node_demo\/index.js
@@ -3,6 +3,7 @@ const s = Server((req, res) => {
   console.log('url: ' + req.url);
   console.log('\nmethod: ' + req.method + '\n');
   res.writeHead(200, { 'Content-length': 3 });
+  res.write('\u0002\u0001\u0021');
   res.write('OK\n');
   res.end()
 });
