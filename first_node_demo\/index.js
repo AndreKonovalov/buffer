40747bd9f0eeb76a6a90c05214fc76812266221b
AndreKonovalov <47122488+AndreKonovalov@users.noreply.github.com>
2020-11-13 13:22:51 +0300

    Update index.js

--- first_node_demo\/index.js
+++ first_node_demo\/index.js
@@ -2,6 +2,8 @@ import { Server } from 'http';
 const s = Server((req, res) => {
   console.log('url: ' + req.url);
   console.log('\nmethod: ' + req.method + '\n');
-  res.end('OK\n')
+  res.writeHead(200, { 'Content-length': 3 });
+  res.write('OK\n');
+  res.end()
 });
 s.listen(4321);
