9ba68349f38ff097628d0e496d910d47c6da36af
AndreKonovalov <47122488+AndreKonovalov@users.noreply.github.com>
2020-11-13 12:36:06 +0300

    Update index.js

--- first_node_demo\/index.js
+++ first_node_demo\/index.js
@@ -1,5 +1,7 @@
 import { Server } from 'http';
 const s = Server((req, res) => {
+  console.log('url: ' + req.url);
+  console.log('\nmethod: ' + req.method + '\n');
   res.end('OK\n')
 });
-s.listen(() => console.log(s.address().port));
+s.listen(4321);
